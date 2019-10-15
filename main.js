f3fd21de92af9194df47393928408c3075c66ae0
DappsUni <43065381+dappsunilabs@users.noreply.github.com>
2019-10-14 21:13:19 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,31 @@
+document.getElementById('input-file')
+  .addEventListener('change', getFile)
+
+function getFile(event) {
+	const input = event.target
+  if ('files' in input && input.files.length > 0) {
+	  placeFileContent(
+      document.getElementById('content-target'),
+      input.files[0])
+  }
+}
+
+function placeFileContent(target, file) {
+	readFileContent(file).then(content => {
+    //console.log(client, content);
+    client.bzz
+      .upload(content,
+              { contentType: 'text/html' })
+       .then(hash => {console.log(hash); target.value=content;})
+  	//target.value = content
+  }).catch(error => console.log(error))
+}
+
+function readFileContent(file) {
+	const reader = new FileReader()
+  return new Promise((resolve, reject) => {
+    reader.onload = event => resolve(event.target.result)
+    reader.onerror = error => reject(error)
+    reader.readAsText(file)
+  })
+}
